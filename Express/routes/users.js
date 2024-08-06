23afb6cac280925df05ad8a6f9ca85b5e2327515
JiaqiJiang1206 <108380174+JiaqiJiang1206@users.noreply.github.com>
2024-08-06 19:58:42 +0800

    add .gitignore

--- Express/routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const userController = require('../controller/user');
-
-/* GET users listing. */
-router.post('/user', userController.saveUserData);
-
-module.exports = router;
--- Vue/src/api/axios.js
+++
@@ -1,9 +0,0 @@
-
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'https://teachernonverbal.asia/api'
-    // baseURL: 'http://localhost:3000/api'
-});
-
-export default instance;
